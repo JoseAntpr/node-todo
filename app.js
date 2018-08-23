8987cbdbe01ea986fd28844e7398560afe68fc3a
JoseAntpr <joseantpalaciosramirez@gmail.com>
2018-08-23 08:22:34 +0200

    ADD: init project & add commands

---
+++ app.js
@@ -0,0 +1,22 @@
+const argv = require('./config/yargs').argv;
+
+console.log(argv);
+
+let comando = argv._[0];
+
+switch( comando ) {
+    case 'crear':
+        console.log('Crear por hacer');
+    break;
+
+    case 'listar':
+        console.log('Mostrar todas las tareas por hacer')
+    break;
+
+    case 'actualizar':
+        console.log('Crear por hacer');
+    break;
+
+    default: 
+         console.log('Comando no reconocido. Elija entre crear | listar | actualizar');
+}
